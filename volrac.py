90fdde33f2ca96f8bc8bfccf78f96ee7ab5ca337
fish@fishisawesome.com <fish@fishisawesome.com>
2017-03-05 10:17:33 +0800

    added flaskmail, and settings

--- volrac.py
+++ volrac.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request
 
 import os
+import settings
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	message = 'Hello world!'
+	message = "Trust me, I'm a coder."
 	return render_template('index.html', message=message)
+
+def about():
+	return render_template('about.html')
+
+def contact():
+	return render_template('contact')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
